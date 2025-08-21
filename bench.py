b4afdc2a94751e37d222b5896314c416abe3a9e7
CursedGreanbeen <101968163+CursedGreanbeen@users.noreply.github.com>
2025-08-22 02:25:59 +0500

    Add files via upload
    Regex host check;
    Empty line check;
    Count > 0 check;
    Comments for better readability

--- bench.py
+++ bench.py
@@ -1,9 +1,10 @@
 import argparse
 import requests
 from collections import Counter
-import regex
+import re
 
 
+# Создание парсера и аргументов
 parser = argparse.ArgumentParser(description='Enter hosts separated with comma without space')
 parser.add_argument('-H', '--hosts', help='Enter hosts', type=str)
 parser.add_argument('-C', '--count', help='Key number', default=1, type=int)
@@ -12,11 +13,13 @@ parser.add_argument('-O', '--output')
 args = parser.parse_args()
 
 
+# Функция, которая обращается по заданному адресу
 def request(url):
     try:
         response = requests.get(url)
         time = response.elapsed.total_seconds()
 
+        # Определяем тип ответа
         if response.status_code < 400:
             return time, 'Success'
         elif 400 <= response.status_code <= 599:
@@ -29,6 +32,7 @@ def request(url):
         return None, 'Error'
 
 
+# Функция, которая собирает и считает статистику по адресу заданное число раз
 def host_info(url, count):
     times = []
     types = []
@@ -61,15 +65,28 @@ def host_info(url, count):
     return total_info
 
 
+# Выполнение скрипта по заданным хостам
 if args.hosts:
+    # Проверка того, что строка состоит как минимум из одного непробельного символа
     if not args.hosts.strip():
         print('Enter hosts using -H or --hosts option')
         exit(1)
+
+    # Разделение строки на отдельные хосты и проверка соответствия заданному шаблону
     separated_hosts = args.hosts.split(',')
     for host in separated_hosts:
-        if re.
+        if not re.match(r'^https?://[0-9A-Za-z-]+\.[A-Za-z]+', host):
+            print(f'{host} is invalid URL. You must use the following pattern: https://example.com')
+            continue
 
-        output = host_info(host, args.count)
+        # Проверка того, что count больше нуля, и запуск функций с учетом введенных параметров
+        if args.count > 0:
+            output = host_info(host, args.count)
+        else:
+            print(f'Invalid count: {args.count}. Set to default value 1')
+            output = host_info(host, 1)
+
+        # Вывод статистики в консоль
         for key, value in output.items():
             print(f"{key}: {value}")
 
